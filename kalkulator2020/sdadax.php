587c99634dcfa82c8986080aabc4fadea007b4d3
nitowahyu01 <54341049+nitowahyu01@users.noreply.github.com>
2020-01-22 11:02:26 +0700

    Add files via upload

---
+++ kalkulator2020/sdadax.php
@@ -0,0 +1,47 @@
+<!--/*
+ *    http://www.opodab.com/
+ *  Use is subject to license terms.
+ */
+-->
+
+<html>
+ <head>
+  <title>Luas Segitiga</title>
+ </head>
+  <body>
+   <form action="rmssgtg.php" method="post">
+   <table width="434" height="86" border="2" bordercolor="#FF0000">
+    <tr>
+     <td align="center" valign="middle"><table width="416" border="0">
+      <tr>
+       <td height="43" colspan="4" align="center" bgcolor="#D0E382"> 
+       <span class="style2"><h2><u><i>Luas Segitiga PHP</u></i></h2></span></td>
+      </tr>
+       <tr>
+        <td height="41" align="center">&nbsp;</td>
+       </tr>
+    <pre>
+     <tr>
+      <td width="117" align="center">
+        Alas:  <input name="alas" type="text" id="nilaiA" size="20" value=""/>
+      </td>
+      <td width="117" align="center">
+        Tinggi:  <input name="tinggi1" type="text" id="nilaiB" size="20" value=""/>
+      </td>      
+     </tr>
+    </pre>
+    <pre>
+   <tr>
+    <td height="43" colspan="4" align="center">
+     <input type="submit" value="Hitung" />
+     <input type="reset" value="Hapus" />  
+    </td>
+   </tr>
+  </pre>
+       </table>
+     </td>
+    </tr>
+   </table>
+   </form>
+  </body>
+</html>
